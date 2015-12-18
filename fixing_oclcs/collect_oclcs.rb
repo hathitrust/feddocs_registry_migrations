604a15177184796c049822b3ea9fb4b8c0e899f0
Joshua Steverman <jstever@applejack.umdl.umich.edu>
2015-12-18 16:33:31 -0500

    adding oclc fields to source records

---
+++ fixing_oclcs/collect_oclcs.rb
@@ -0,0 +1,56 @@
+require 'registry_record'
+require 'source_record'
+require_relative '../header' 
+require 'pp'
+require 'mongo'
+require 'dotenv'
+
+Dotenv.load
+
+OCLCPAT = 
+  /
+      \A\s*
+      (?:(?:\(OCoLC\)) |
+  (?:\(OCoLC\))?(?:(?:ocm)|(?:ocn)|(?:on))
+  )(\d+)
+  /x
+
+Mongoid.load!("config/mongoid.yml", :development)
+Mongo::Logger.logger.level = ::Logger::FATAL
+
+@mc = Mongo::Client.new([ENV['mongo_host']+':'+ENV['mongo_port']], :database => 'htgd' )
+
+recs = SourceRecord.where(:stated_oclcnum.exists => false )
+recs.each do | rec |
+  fields = {}
+  rec[:source]["fields"].each do | f | 
+    k = f.keys[0]
+    if fields[k]
+      fields[k].push f[k]
+    else 
+      fields[k] = [f[k]]
+    end
+  end
+
+  if fields["003"] and fields["003"][0] =~ /OCoLC/i 
+    rec.oclc_location = "001"
+    rec.stated_oclcnum = fields["001"][0].gsub(/\D/, '')
+  else
+    fields["035"].each do | f | 
+      as = f["subfields"].select { | sf | sf.keys[0] == "a" }
+      zs = f["subfields"].select { | sf | sf.keys[0] == "z" }
+      if as and OCLCPAT.match(as[0]["a"]) 
+        rec.stated_oclcnum = $1
+
+        zs.each do | z |
+          if OCLCPAT.match(z["z"])
+            @mc[:oclc_resolution].insert_one({:canceled => $1, :replacement => rec.stated_oclcnum})
+          end
+        end
+      end
+    end
+  end
+
+  rec.save
+  exit
+end
