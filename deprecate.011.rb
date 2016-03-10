03b588c8a2140c9debcc100eca576f1fdc04bd72
Josh Steverman <jstever@umich.edu>
2016-03-10 18:19:43 -0500

    minor change to deprecate011

--- deprecate.011.rb
+++ deprecate.011.rb
@@ -3,9 +3,8 @@ require 'source_record'
 require './header' 
 
 #Michigan State has ~220k records that are just HT record links
-recs = SourceRecord.where(:org_code => "miem", :source_blob => /001...hth/)
 source_count = 0
-recs.each do | rec | 
+SourceRecord.where(:org_code => "miem", :source_blob => /001...hth/).no_timeout.each do | rec | 
   source_count += 1 
   rec.deprecate("#{REPO_VERSION}: Records (partially) copied from HathiTrust.")
   #deprecate a related registry record if it's the only source for it.
