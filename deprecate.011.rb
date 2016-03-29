bc262681105e9f1b1203d84173ae1f2542a56595
Josh Steverman <jstever@umich.edu>
2016-03-28 21:54:39 -0400

    track regrecs deprecated

--- deprecate.011.rb
+++ deprecate.011.rb
@@ -4,14 +4,17 @@ require './header'
 
 #Michigan State has ~220k records that are just HT record links
 source_count = 0
+reg_count = 0
 SourceRecord.where(:org_code => "miem", :source_blob => /001...hth/).no_timeout.each do | rec | 
   source_count += 1 
   rec.deprecate("#{REPO_VERSION}: Records (partially) copied from HathiTrust.")
   #deprecate a related registry record if it's the only source for it.
   RegistryRecord.where(:source_record_ids => [rec.source_id]).each do | regrec |
+    reg_count += 1
     regrec.deprecate("#{REPO_VERSION}: Records (partially) copied from HathiTrust.")
   end
 
 end
 puts "MSU/HT Records deprecated: #{source_count}"
+puts "RegRecs deprecated: #{reg_count}"
 
