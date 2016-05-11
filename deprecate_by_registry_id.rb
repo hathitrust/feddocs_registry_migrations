3161c5f5c06593a913e05a8d90a2055f5e1399d1
Josh Steverman <jstever@umich.edu>
2016-05-11 16:05:30 -0400

    ignore non-existent registry ids

--- deprecate_by_registry_id.rb
+++ deprecate_by_registry_id.rb
@@ -12,16 +12,19 @@ fin.each do | line |
   #extract the registry id in case it's a full url
   reg_id = /(?:.*catalog\/)?(.*)(?:#)?/.match(line.split(/\t/)[0])[1]  
   if reg_id.length != 36
-    puts "bad reg id: #{reg_id}"
+    #puts "bad reg id: #{reg_id}"
     next
   end
 
-  RegistryRecord.where(registry_id: reg_id,
-                       deprecated_timestamp: {"$exists":0})
-                       .no_timeout.each do | regrec | 
-    reg_count += 1
-    regrec.deprecate("#{REPO_VERSION}: Not a US Federal Document. Identified by registry identifier.")
+  regrec = RegistryRecord.where(registry_id: reg_id,
+                               deprecated_timestamp: {"$exists":0}).first
+  if regrec.nil?
+    #non existent registry record
+    next
   end
+
+  reg_count += 1
+  regrec.deprecate("#{REPO_VERSION}: Not a US Federal Document. Identified by registry identifier.")
 end
 
 puts "RegRecs deprecated: #{reg_count}"
