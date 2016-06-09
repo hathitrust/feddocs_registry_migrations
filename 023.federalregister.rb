cee4fee045e75e6fe20c833b3ee24cf165975724
Josh Steverman <jstever@umich.edu>
2016-06-09 17:32:55 -0400

    add series statement to fed register records

---
+++ 023.federalregister.rb
@@ -0,0 +1,37 @@
+require 'registry_record'
+require 'source_record'
+require './header' 
+require 'pp'
+
+# Add series info to Federal Register source records 
+
+source_count = 0
+
+oclcnums = [1768512,
+            3803349,
+            9090879,
+            6141934,
+            27183168,
+            9524639,
+            60637209,
+            25816139,
+            27163912,
+            7979808,
+            4828080,
+            18519766,
+            41954100,
+            43080713,
+            38469925,
+            97118565,
+            70285150 ]
+
+
+
+SourceRecord.where(oclc_resolved:{"$in":oclcnums}).no_timeout.each do |src|
+  source_count += 1
+  src.series = "FederalRegister"
+  src.save
+end
+
+puts "Source records: #{source_count}"
+
