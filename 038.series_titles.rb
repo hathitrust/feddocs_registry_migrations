691fd46c79864a403344f63fd2472c9aabe1f048
Josh Steverman <jstever@umich.edu>
2016-10-03 16:24:10 -0400

    add series names to records

---
+++ 038.series_titles.rb
@@ -0,0 +1,112 @@
+require 'registry_record'
+require 'source_record'
+require './header' 
+require 'pp'
+
+# Add series info to source and registry records 
+# needless repetition I know
+
+# Economic Report of the President
+source_count = 0
+rr_count = 0
+SourceRecord.where(sudocs:/^#{RegExp.escape(EconomicReportOfThePresident.sudoc_stem}/).no_timeout.each do |src|
+  source_count += 1
+  src.series = "EconomicReportOfThePresident"
+  src.save
+  RegistryRecord.where(source_record_ids:src.source_id, 
+                       series:{"$ne":"Economic Report Of The President"}).no_timeout.each do |r|
+    r.series = "Economic Report Of The President"
+    r.save
+  end
+end
+puts "ERPres sources: #{source_count}"
+puts "ERPres RR: #{rr_count}"
+
+source_count = 0
+rr_count = 0
+#United States Reports
+SourceRecord.where(oclc_resolved:{"$in":UnitedStatesReports.oclcs}).no_timeout.each do |src|
+  source_count += 1
+  src.series = "UnitedStatesReports"
+  src.save
+  RegistryRecord.where(source_record_ids:src.source_id, 
+                       series:{"$ne":"United States Reports"}).no_timeout.each do |r|
+    r.series = "United States Reports"
+    rr_count += 1
+    r.save
+  end
+
+end
+puts "USReports: #{source_count}"
+puts "USReports rr: #{rr_count}"
+
+#Foreign Relations of the United States
+source_count = 0
+rr_count = 0
+SourceRecord.where(sudocs:/^#{RegExp.escape(ForeignRelations.sudoc_stem}/).no_timeout.each do |src|
+  source_count += 1
+  src.series = "ForeignRelations"
+  src.save
+  RegistryRecord.where(source_record_ids:src.source_id, 
+                       series:{"$ne":"Foreign Relations"}).no_timeout.each do |r|
+    r.series = "Foreign Relations"
+    r.save
+  end
+
+end
+puts "FR sources: #{source_count}"
+puts "Foreign Relations RR: #{rr_count}"
+
+#Congressional Record
+source_count = 0
+rr_count = 0
+SourceRecord.where(sudocs:/^#{RegExp.escape(CongressionalRecord.sudoc_stem}/).no_timeout.each do |src|
+  source_count += 1
+  src.series = "CongressionalRecord"
+  src.save
+  RegistryRecord.where(source_record_ids:src.source_id, 
+                       series:{"$ne":"Congressional Record"}).no_timeout.each do |r|
+    r.series = "Congressional Record"
+    rr_count += 1
+    r.save
+  end
+
+end
+puts "Congressional Record source: #{source_count}"
+puts "congressional Record RR: #{rr_count}"
+
+#Congressional Serial Set
+source_count = 0
+rr_count = 0
+SourceRecord.where(sudocs:/^#{RegExp.escape(CongressionalSerialSet.sudoc_stem}/).no_timeout.each do |src|
+  source_count += 1
+  src.series = "CongressionalSerialSet"
+  src.save
+  RegistryRecord.where(source_record_ids:src.source_id, 
+                       series:{"$ne":"Congressional Serial Set"}).no_timeout.each do |r|
+    r.series = "Congressional Serial Set"
+    rr_count += 1
+    r.save
+  end
+end
+puts "Congressional Record source: #{source_count}"
+puts "congressional Record RR: #{rr_count}"
+
+#Civil Rights Commission
+source_count = 0
+rr_count = 0
+SourceRecord.where(sudocs:/^#{RegExp.escape(CivilRightsCommission.sudoc_stem}/).no_timeout.each do |src|
+  source_count += 1
+  src.series = "CivilRightsCommission"
+  src.save
+  RegistryRecord.where(source_record_ids:src.source_id, 
+                       series:{"$ne":"Civil Rights Commission"}).no_timeout.each do |r|
+    r.series = "Civil Rights Commission"
+    rr_count += 1
+    r.save
+  end
+end
+puts "Civil Rights Commission source: #{source_count}"
+puts "Civil Rights Commission RR: #{rr_count}"
+
+
