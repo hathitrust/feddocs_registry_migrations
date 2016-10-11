fcae64bc3d8013c6194d2f9ef8994c9f2b4a9459
Josh Steverman <jstever@umich.edu>
2016-10-11 14:30:00 -0400

    fix EconReport enumchrons

--- 039.econreports.rb
+++ 039.econreports.rb
@@ -22,6 +22,19 @@ end
 SourceRecord.where(series: "EconomicReportOfThePresident",
                    deprecated_timestamp:{"$exists":0}).no_timeout.each do |src|
   source_count += 1
+
+  # wth flasus
+  if src.org_code == 'flasus'
+    f = src.source['fields'].find {|f| f['955'] }['955']['subfields']
+    v = f.select { |h| h['v'] }[0]
+    junk_sf = f.select { |h| h.keys[0] =~ /\./ }[0]
+    if !junk_sf.nil?
+      junk = junk_sf.keys[0]
+      v['v'] = junk
+      f.delete_if { |h| h.keys[0] =~ /\./ }
+    end
+  end
+
   src.source = src.source.to_json
   src.enum_chrons.each do | ec | 
     rr_count += 1
