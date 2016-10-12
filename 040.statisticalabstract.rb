1d85ef0e7e651a467c5c0237a4f71a6008244c30
Josh Steverman <jstever@umich.edu>
2016-10-12 13:09:24 -0400

    cleaning up after the Registry 0.9.2 bug

--- 040.statisticalabstract.rb
+++ 040.statisticalabstract.rb
@@ -5,21 +5,26 @@ require 'pp'
 
 include Registry
 include Registry::Series
-# Parse enumchrons for Economic Report registry records 
-
+# Parse enumchrons for Statistical Abstract registry records 
+# Previous enumchron_extraction code was buggy, creating duplicate enumchrons
+# instead of sequential(exploded) enumchrons. Registry v. 0.9.2
+#
 deprecate_count = 0
 source_count = 0
 rr_count = 0
 
-# Deprecate all the EconReport RegRecs and start again with SourceRecords
-RegistryRecord.where(series:"Economic Report Of The President",
+=begin
+# Usually we would deprecate existing RegRecs, but the bug missed enumchrons, 
+# but didn't create erroneous ones. 
+RegistryRecord.where(series:"Statistical Abstract",
                      deprecated_timestamp:{"$exists":0}).no_timeout.each do |reg|
-  reg.deprecate('Improved Economic Report enum/chron parsing.')
+  reg.deprecate('Improved Statistical Abstract enum/chron parsing.')
   deprecate_count += 1
 end
+=end
 
 # Re-extract all the Source Records
-SourceRecord.where(series: "EconomicReportOfThePresident",
+SourceRecord.where(series: "StatisticalAbstract",
                    deprecated_timestamp:{"$exists":0}).no_timeout.each do |src|
   source_count += 1
 
@@ -37,15 +42,15 @@ SourceRecord.where(series: "EconomicReportOfThePresident",
 
   src.source = src.source.to_json
   src.enum_chrons.each do | ec | 
-    if regrec = RegistryRecord.where(series:"Economic Report Of The President", 
+    if regrec = RegistryRecord.where(series:"Statistical Abstract", 
                                      deprecated_timestamp:{"$exists":0}, 
                                      enumchron_display:ec).first
       regrec.add_source(src)
     else
-      regrec = RegistryRecord.new([src.source_id], ec, "Improved Econ Report enum/chron parsing.")
+      regrec = RegistryRecord.new([src.source_id], ec, "Improved Statistical Abstract enum/chron parsing.")
       rr_count += 1
     end
-    regrec.series = "Economic Report Of The President"
+    regrec.series = "Statistical Abstract"
     regrec.save
   end
   src.save
