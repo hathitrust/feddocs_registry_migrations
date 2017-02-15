1903ba0cd8de5095802e7f0edd1995183b4c857e
Josh Steverman <jstever@umich.edu>
2017-02-15 13:27:18 -0500

    fix flasus

--- 056.default_ec_handling.rb
+++ 056.default_ec_handling.rb
@@ -18,10 +18,26 @@ srcs_updated = 0
 
 open(ARGV.shift).each do | line |
   src_id = line.chomp
-
+  begin
   src = SourceRecord.where(source_id: src_id).no_timeout.first
+
+  # wth flasus?
+  if src.org_code == 'flasus'
+    f = src.source['fields'].find {|f| f['955'] }['955']['subfields']
+    v = f.select { |h| h['v'] }[0]
+    junk_sf = f.select { |h| h.keys[0] =~ /\./ }[0]
+    if !junk_sf.nil?
+      junk = junk_sf.keys[0]
+      v['v'] = junk
+      f.delete_if { |h| h.keys[0] =~ /\./ }
+    end
+  end
+  
   src.source = src.source.to_json # re-extract enumchrons here
   src.save
+  rescue
+    PP.pp src
+  end
   #we've already done this, because we started with the registry in 055
   # res = src.update_in_registry(" yada yada yada ") 
   
