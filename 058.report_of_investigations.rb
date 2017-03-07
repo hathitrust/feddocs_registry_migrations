15d6d106944eeda4856284f0de0f0e72fa153070
Josh Steverman <jstever@umich.edu>
2017-03-07 18:35:55 -0500

    use the right name for Reports of Investigations

--- 058.report_of_investigations.rb
+++ 058.report_of_investigations.rb
@@ -5,21 +5,21 @@ require 'pp'
 
 include Registry
 include Registry::Series
-# Parse enumchrons for Report of Investigations registry records 
+# Parse enumchrons for Reports of Investigations registry records 
 #
 =begin
 =end
 #
 source_count = 0
 rr_count = 0
-SourceRecord.where(oclc_resolved:{"$in":ReportOfInvestigations.oclcs}, 
-                  series:{"$ne":"ReportOfInvestigations"}).no_timeout.each do |src|
+SourceRecord.where(oclc_resolved:{"$in":ReportsOfInvestigations.oclcs}, 
+                  series:{"$ne":"ReportsOfInvestigations"}).no_timeout.each do |src|
   source_count += 1
-  src.series = "ReportOfInvestigations"
+  src.series = "ReportsOfInvestigations"
   src.save
   RegistryRecord.where(source_record_ids:src.source_id, 
-                       series:{"$ne":"Report Of Investigations"}).no_timeout.each do |r|
-    r.series = "Report Of Investigations"
+                       series:{"$ne":"Reports Of Investigations"}).no_timeout.each do |r|
+    r.series = "Reports Of Investigations"
     rr_count += 1
     r.save
   end
@@ -30,7 +30,7 @@ puts "Initial RR count: #{rr_count}"
 deprecate_count = 0
 rr_count = 0
 
-SourceRecord.where(series:"ReportOfInvestigations", 
+SourceRecord.where(series:"ReportsOfInvestigations", 
                    deprecated_timestamp:{"$exists":0}).no_timeout.each do |src|
   src.source = src.source.to_json #re-extraction done here
   res = src.update_in_registry("Improved enum/chron parsing. #{REPO_VERSION}") #this will take care of everything
