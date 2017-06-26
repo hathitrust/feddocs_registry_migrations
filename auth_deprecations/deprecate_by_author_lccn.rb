061ba5b366e14dedb930c668845f292affd9f22c
Josh Steverman <jstever@umich.edu>
2017-06-26 11:59:37 -0400

    basics of deprecation by author_lccns field

---
+++ auth_deprecations/deprecate_by_author_lccn.rb
@@ -0,0 +1,39 @@
+require 'registry_record'
+require 'source_record'
+require './header' 
+
+
+count = 0
+nongd = 0
+regs = {}
+reg_count = 0
+
+author_lccn = ARGV.shift
+if author_lccn !~ /http/
+  exit
+end
+srcs = SourceRecord.where({deprecated_timestamp:{"$exists":0}, 
+                           author_lccns:author_lccn})
+
+srcs.each do | src |
+  count += 1
+  if src.is_govdoc
+    next
+  else
+    nongd += 1
+    RegistryRecord.where({deprecated_timestamp:{"$exists":0}, source_record_ids:src.source_id}).each do | reg |
+      reg_count += 1
+      reg.deprecate("#{REPO_VERSION}: Not a United States Federal Document. Identified by author.")
+      if !regs.has_key?(reg.registry_id)
+        regs[reg.registry_id] = reg.source_record_ids
+      end
+    end
+    src.deprecate("#{REPO_VERSION}: Not a United States Federal Document. Identified by author.")
+    #bad_out.puts src.source.to_json
+  end
+  
+end
+
+puts "# source records with non-sudocs: #{count}"
+puts "# nongd sources: #{nongd}"
+puts "# of related Registry Records: #{reg_count}"
