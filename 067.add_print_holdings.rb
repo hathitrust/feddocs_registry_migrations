018ed66c356d93fe1fe85d139f0a3c472728aeaa
Josh Steverman <jstever@umich.edu>
2017-07-07 11:43:31 -0400

    need oclcnum to look up print holdings

--- 067.add_print_holdings.rb
+++ 067.add_print_holdings.rb
@@ -9,7 +9,8 @@ Dotenv.load!
 Mongoid.load!(File.expand_path("../config/mongoid.yml", __FILE__), :development)
 num_rr = 0
 RegistryRecord.where(deprecated_timestamp:{"$exists":0},
-                     print_holdings_t:{"$exists":0}).no_timeout.each do |reg|
+                     print_holdings_t:{"$exists":0},
+                     "oclcnum_t.0":{"$exists":1}).no_timeout.each do |reg|
   num_rr += 1
   reg.print_holdings
   reg.save
