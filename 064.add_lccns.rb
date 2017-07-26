d1e65745e955974d594315b92af5a3515885ed54
Josh Steverman <jstever@umich.edu>
2017-07-26 16:14:03 -0400

    save reextracted lccns if there are more of them

--- 064.add_lccns.rb
+++ 064.add_lccns.rb
@@ -9,20 +9,24 @@ Dotenv.load!
 Mongoid.load!(File.expand_path("../config/mongoid.yml", __FILE__), :development)
 num_src = 0
 # Extract heading entries 100:110 and  700:710
-SourceRecord.where(deprecated_timestamp:{"$exists":0},
-		   author_lccns:{"$exists":0}).no_timeout.each do | src |
-  num_src += 1
+SourceRecord.where(deprecated_timestamp:{"$exists":0}).no_timeout.each do | src |
+  prev_count = src['author_lccns'].count
   src.author_lccns
   src.added_entry_lccns
-  src.save
+  if prev_count <= src['author_lccns'].count
+    num_src += 1
+    src.save
+  end
 end 
 num_rr = 0
-RegistryRecord.where(deprecated_timestamp:{"$exists":0},
-                     author_lccns:{"$exists":0}).no_timeout.each do |reg|
-  num_rr += 1
+RegistryRecord.where(deprecated_timestamp:{"$exists":0}).no_timeout.each do |reg|
+  prev_count = reg['author_lccns']
   reg.author_lccns = reg.sources.collect{|s| s.author_lccns}.flatten.uniq
   reg.added_entry_lccns = reg.sources.collect{|s| s.added_entry_lccns}.flatten.uniq
-  reg.save
+  if prev_count <= reg['author_lccns']
+    num_rr += 1
+    reg.save
+  end
 end 
 
 puts "num src records re-extracted: #{num_src}"
