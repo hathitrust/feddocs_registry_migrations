8038758e57839cae59267ca0f59306f0e7c13a4c
Josh Steverman <jstever@umich.edu>
2019-03-11 19:25:56 -0400

    Handbook of North American Indians

---
+++ 112.handbook.rb
@@ -0,0 +1,42 @@
+require 'registry/registry_record'
+require 'registry/source_record'
+require 'ecmangle'
+require './header' 
+require 'pp'
+
+include Registry
+# Parse enumchrons for Handbook of NA Indians registry records 
+#
+ocns = [13240086, 568690282, 622355838, 670012899, 679862056]
+
+source_count = 0
+rr_count = 0
+SourceRecord.where(oclc_resolved:{"$in":ocns}).no_timeout.each do |src|
+  source_count += 1
+  src.series = src.series
+  src.save
+  RegistryRecord.where(source_record_ids:src.source_id, 
+                       series:{"$ne":'Handbook of North American Indians'},
+                       deprecated_timestamp:{"$exists":0}
+                      ).no_timeout.each do |r|
+    rr_count += 1
+    r.series = r.sources.collect(&:series).flatten.uniq
+    r.save
+  end
+end
+puts "Handbook sources: #{source_count}"
+puts "Initial RR count: #{rr_count}"
+deprecate_count = 0
+rr_count = 0
+
+SourceRecord.where(series:'Handbook of North American Indians', 
+                   deprecated_timestamp:{"$exists":0}).no_timeout.each do |src|
+  src.source = src.source.to_json #re-extraction done here
+  res = src.update_in_registry("Improved enum/chron parsing. #{REPO_VERSION}") #this will take care of everything
+  deprecate_count += res[:num_deleted]
+  rr_count += res[:num_new]
+  src.save
+end
+puts "# new RegRecs: #{rr_count}"
+puts "Deprecated records: #{deprecate_count}"
+puts "Source records: #{source_count}"
