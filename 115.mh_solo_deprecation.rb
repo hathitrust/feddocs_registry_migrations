38feca476a1199864a9f6700b6b717a2d6e30770
Josh Steverman <jstever@umich.edu>
2019-03-25 18:23:41 -0400

    115 complete

--- 115.mh_solo_deprecation.rb
+++ 115.mh_solo_deprecation.rb
@@ -19,6 +19,7 @@ SR.where(org_code:"mh",
   # is it solo in the Registry
   next if RR.where(source_record_ids:[src.source_id],
                                deprecated_timestamp:{"$exists":0}).count == 0
+  src.remove_from_registry("Does not appear to be a US Fed Doc. #{REPO_VERSION}")
   src.deprecate("Does not appear to be a US Fed Doc. #{REPO_VERSION}")
   num_deprecated += 1
 end
