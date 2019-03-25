797e20f019c78ad1e140f6a3d2e09b27b3f12828
Josh Steverman <jstever@umich.edu>
2019-03-25 14:31:05 -0400

    MH solo deprecation ht-1162

---
+++ 115.mh_solo_deprecation.rb
@@ -0,0 +1,24 @@
+require 'registry/registry_record'
+require 'registry/source_record'
+require 'ecmangle'
+require './header' 
+require 'pp'
+
+SourceRecord = Registry::SourceRecord
+
+# JIRA issue HT-1162
+# MH records got dragged in, presumably by INND records. Now that they are 
+# deprecated we need to deprecate MH records that are solos and no indication
+# of being fed docs. 
+num_deprecated = 0
+SourceRecord.where(org_code:"mh",
+                   in_registry:true,
+                   deprecated_timestamp:{"$exists":0}).no_timeout.each do | src |
+  next if src.fed_doc?
+  # is it solo in the Registry
+  next if RegistryRecord.where(source_record_ids:[src.source_id],
+                               deprecated_timestamp:{"$exists":0}).count == 0
+  src.deprecate("Does not appear to be a US Fed Doc. #{REPO_VERSION}")
+  num_deprecated += 1
+end
+puts "num deprecated:#{num_deprecated}"
