26a2d2679580dfc3c42c3f3a12bd3b38249b9269
Josh Steverman <jstever@umich.edu>
2019-08-16 14:18:14 -0400

    US Exports D and F

---
+++ 122.us_exports_domestic_foreign.rb
@@ -0,0 +1,42 @@
+require 'registry/registry_record'
+require 'registry/source_record'
+require 'ecmangle'
+require './header' 
+require 'pp'
+
+include Registry
+# Parse enumchrons for US Exports of Domestic and Foreign Merchandise registry records 
+#
+ocns = [ 7592910, 557737451 ]
+
+source_count = 0
+rr_count = 0
+SourceRecord.where(oclc_resolved:{"$in":ocns}).no_timeout.each do |src|
+  source_count += 1
+  src.series = src.series
+  src.save
+  RegistryRecord.where(source_record_ids:src.source_id, 
+                       series:{"$ne":'United States Exports of Domestic and Foreign Merchandise'},
+                       deprecated_timestamp:{"$exists":0}
+                      ).no_timeout.each do |r|
+    rr_count += 1
+    r.series = r.sources.collect(&:series).flatten.uniq
+    r.save
+  end
+end
+puts "USE sources: #{source_count}"
+puts "Initial RR count: #{rr_count}"
+deprecate_count = 0
+rr_count = 0
+
+SourceRecord.where(series:'United States Exports of Domestic and Foreign Merchandise', 
+                   deprecated_timestamp:{"$exists":0}).no_timeout.each do |src|
+  src.source = src.source.to_json #re-extraction done here
+  res = src.update_in_registry("Improved enum/chron parsing. #{REPO_VERSION}") #this will take care of everything
+  deprecate_count += res[:num_deleted]
+  rr_count += res[:num_new]
+  src.save
+end
+puts "# new RegRecs: #{rr_count}"
+puts "Deprecated records: #{deprecate_count}"
+puts "Source records: #{source_count}"
