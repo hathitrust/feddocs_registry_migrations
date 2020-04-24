c345cdc5bf73c6db09062104eb6db327b7346caa
Josh Steverman <jstever@umich.edu>
2020-04-24 14:22:01 -0400

    Update holdings in miaahdl records

---
+++ 127.update_ht_holdings.rb
@@ -0,0 +1,20 @@
+require 'registry/registry_record'
+require 'registry/source_record'
+require './header' 
+require 'pp'
+
+include Registry
+
+# We have been updating Enum Chron parsing but not the holdings data for HT
+# records. This leaves a mismatch in ec strings 
+
+srcs_reextracted = 0
+SourceRecord.where(org_code:"miaahdl",
+                   deprecated_timestamp:{"$exists":0}
+                  ).no_timeout.each do |src|
+  srcs_reextracted += 1
+  src.extract_holdings
+  src.save
+end 
+
+puts "srcs re extracted:#{srcs_reextracted}"
